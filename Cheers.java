7060092f2d3af3d6fb94c2ba6237b99748f79b1a
OhadZvi <ohad.zvi@post.runi.ac.il>
2024-11-14 14:53:06 +0200

    task completed

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,18 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String input = args[0].toUpperCase();
+            String anLetters = "AEFHILMNORSX";
+            for (int i = 0; i < input.length(); i++) {
+                System.out.print("Give me a");
+                if (anLetters.indexOf(input.charAt(i)) == -1) {
+                        System.out.print("n");                       
+                }
+                System.out.println(" " + input.charAt(i) + ": " + input.charAt(i) + "!");
+            }
+            System.out.println("What does that spell?");
+            for (int i = 0; i < Integer.parseInt(args[1]); i++) {
+                System.out.println(input + "!!!");
+            }
         }
 }
