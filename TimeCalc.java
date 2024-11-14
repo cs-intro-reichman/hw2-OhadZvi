88a31efe068dbaac29caf9d497ad444174f63185
OhadZvi <ohad.zvi@post.runi.ac.il>
2024-11-14 13:22:22 +0200

    completed task

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,21 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        String[] inpTime = args[0].split(":");
+        int inpHours = Integer.parseInt(inpTime[0]);
+        int inpMinutes = Integer.parseInt(inpTime[1]);
+        int minutesToAdd = Integer.parseInt(args[1]);
+        int totMinutes = inpHours * 60 + inpMinutes + minutesToAdd;
+        int totHours = totMinutes / 60;
+        int newHours = totHours % 24;
+        int newMinutes = totMinutes - (totHours * 60);
+        String sNewHours  = "" + newHours;
+        String sNewMinutes = "" + newMinutes;
+        if (newHours < 10) {
+            sNewHours = "0" + newHours;
+        }
+        if (newMinutes < 10) {
+            sNewMinutes = "0" + newMinutes;
+        }
+        System.out.println(sNewHours + ":" + sNewMinutes);
     }
 }
